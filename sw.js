71ab4c2815ba79da21ffb21daa4ae4e0e8aa4b0c
leocotrimtrabalhos <leocotrimtrabalhos@gmail.com>
2026-05-16 11:24:40 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,8 +13,8 @@ const ASSETS = [
   './styles.css',
   './app.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // Instalação: pré-cacheia todos os assets críticos.
